ad66add4d511f5e2239264164655ec3e8292231f
FrederikTheDane <13228251+FrederikTheDane@users.noreply.github.com>
2018-09-13 11:25:31 +0200

    Various changes (see desc.)
    Added a stat stage entry to the PokeMon and PokeMove structs.
    Set max pkmn level to 100.
    Fixed the EV adding func.
    Improved readability for the stat calculator.

--- src/github.com/frederikthedane/discordmon/funcs.go
+++ src/github.com/frederikthedane/discordmon/funcs.go
@@ -6,12 +6,36 @@ func init() {
 	Table = map[int]PokeMonBase{}
 }
 
+//Sums ints in an array
 func sumInts(ints []int) int {
 	sum := 0
 	for _, v := range ints {
 		sum += v
 	}
 	return sum
+}
+
+//Sums the values in two arrays
+func sumArrays(arr1, arr2 []int) []int {
+	if len(arr1) != len(arr2) {
+		panic("Panic! The array lengths do not match!")
+	}
+	sumArr := make([]int, len(arr1))
+	for k := range sumArr {
+		sumArr[k] = arr1[k] + arr2[k]
+	}
+	return sumArr
+}
+
+//Gets key of the largest int in an array
+func getLargestKey(ints []int) int {
+	largest := 0
+	for k, v := range ints {
+		if v > ints[largest] {
+			largest = k
+		}
+	}
+	return largest
 }
 
 //Get a new PokeMon with the given numeric ID
--- src/github.com/frederikthedane/discordmon/structs.go
+++ src/github.com/frederikthedane/discordmon/structs.go
@@ -24,73 +24,76 @@ type PokeMon struct {
 	Base PokeMonBase
 	EVS [6]int
 	IVS [6]int
+	StatStages [6]int
 	Stats [6]int
 	Moves [4]PokeMove
 	Gender, NVStatus, VStatus, Level, Nature int
 	PokeRus bool
 }
 
+//Sets the pokemon to a given level, and calculates its new stats
 func (mon *PokeMon) SetLevel(lvl int) {
-	mon.Level = lvl
+	if lvl > 100 {
+		mon.Level = 100
+	} else {
+		mon.Level = lvl
+	}
 	mon.Stats = mon.CalculateStats()
 }
 
-//TODO: Fix this so it will round down if the new EV's exceed maximum
+//Rounds down if the new EV's exceed maximum. 252 for each stat max, 510 for all
 func (mon *PokeMon) GainEVs(evs [6]int) {
-	tempEVS := mon.EVS
 
 	exceedsMaxEVS := func(newEVs []int) bool {
-		if sumInts(newEVs) > 510 {
-			return true
-		} else {
-			return false
+		for _, v := range newEVs {
+			if v > 252 {
+				return true
+			}
 		}
+		return sumInts(newEVs) > 510
 	}
 
-	for i := 0; i < len(evs); i++ {
-		tempEVS[i] += evs[i]
-		nextTry := tempEVS
-		nextTry[i] -= 1
-		finalTry := nextTry
-		finalTry[i] -= 1
-		if !exceedsMaxEVS(tempEVS[:]) && tempEVS[i] + evs[i] <= 252{
-			mon.EVS[i] += evs[i]
-		} else if nextTry[i] + evs[i] <= 252 {
-			mon.EVS[i] += nextTry[i]
-		} else if finalTry[i] + evs[i] <= 252 {
-			mon.EVS[i] += finalTry[i]
-		}
-	}
+	var addEVs func(toAdd []int) []int
+	addEVs = func(toAdd []int) []int {
+		adder := toAdd
+		tempEVs := toAdd
+		if exceedsMaxEVS(sumArrays(mon.EVS[:], tempEVs[:])) {
+			tempEVs[getLargestKey(toAdd[:])] -= 1
+			adder = addEVs(tempEVs)
 
+		}
+		return adder
+	}
+	copy(mon.EVS[:], sumArrays(addEVs(evs[:]), mon.EVS[:]))
 	mon.Stats = mon.CalculateStats()
 }
 
 //Calculates a PokeMons stats (including hp) from lvl, iv's ev's and base stats
 func (mon *PokeMon) CalculateStats() [6]int {
 	stats := *new([6]int)
-	stats[0] = mon.CalculateHP()
+	stats[StatHP] = mon.CalculateHP()
 	lvl := mon.Level
 	for i := 1; i < 6; i++ {
 		bsStat := mon.Base.BaseStats[i]
 		ivStat := mon.IVS[i]
 		evStat := mon.EVS[i]
 		natureMod := float64(Natures[mon.Nature][i])
-		stats[i] = int(math.Floor(math.Floor(float64((2 * bsStat + ivStat + evStat) * lvl / 100 + 5) * natureMod)))
+		stats[i] = int(math.Floor(float64((2 * bsStat + ivStat + evStat) * lvl / 100 + 5) * natureMod))
 	}
 	return stats
 }
 
-//Same as above, but for only HP as the formula is a bit different
+//Same as above, but only for HP, as the formula is a bit different
 func (mon *PokeMon) CalculateHP() int {
-	bsHP := mon.Base.BaseStats[0]
-	ivHP := mon.IVS[0]
-	evHP := mon.EVS[0]
+	bsHP := mon.Base.BaseStats[StatHP]
+	ivHP := mon.IVS[StatHP]
+	evHP := mon.EVS[StatHP]
 	lvl := mon.Level
 	stat := int(math.Floor(float64((2 * bsHP + ivHP + evHP) * lvl / 100 + lvl + 10)))
 	return stat
 }
 
-//TODO: Test this func. It calculates the effectiveness compared to the RECEIVING pokemon
+//Calculates the effectiveness compared to the RECEIVING pokemon
 func (mon *PokeMon) CalculateDamageMultiplier(pokeType PokeType) float64 {
 	types := mon.Base.Types
 	multiplier := 1.0
@@ -115,6 +118,8 @@ type PokeMove struct {
 	Accuracy float64
 	Priority int
 	Contact bool
+	OwnStatChanges [6]int
+	OppStatChanges [6]int
 }
 
 type PokeType struct {
