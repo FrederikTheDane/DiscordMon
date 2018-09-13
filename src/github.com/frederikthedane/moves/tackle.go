99de4fa053e1a7921d0d45adddfac4551b7bd048
FrederikTheDane <13228251+FrederikTheDane@users.noreply.github.com>
2018-09-13 11:27:00 +0200

    Added some spacing

--- src/github.com/frederikthedane/moves/tackle.go
+++ src/github.com/frederikthedane/moves/tackle.go
@@ -3,11 +3,11 @@ package discordmonMoves
 import "github.com/frederikthedane/discordmon"
 
 var TackleMove = discordmon.PokeMove{
-	Type:     discordmon.TypeNormal,
-	Category: discordmon.CategoryPhysical,
-	PP:       35,
-	Power:    40,
-	Accuracy: 1.0,
-	Priority: 0,
-	Contact:  true,
+	Type:           discordmon.TypeNormal,
+	Category:       discordmon.CategoryPhysical,
+	PP:             35,
+	Power:          40,
+	Accuracy:       1.0,
+	Priority:       0,
+	Contact:        true,
 }
